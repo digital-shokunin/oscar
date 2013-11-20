badf4c5f94205072682537544d2aba71e3eb85e5
David <surge.mitchell@gmail.com>
2013-11-20 14:53:21 -0500

    Update print statements for Python 3

--- scan.py
+++ scan.py
@@ -130,10 +130,10 @@ def add_grocery_item(trello_api, item):
 
     # Add item if it's not there already
     if item not in card_names:
-        print "Adding '{0}' to grocery list".format(item)
+        print("Adding '{0}' to grocery list".format(item))
         trello_api.lists.new_card(grocery_list['id'], item)
     else:
-        print "Item '{0}' is already on the grocery list; not adding".format(item)
+        print("Item '{0}' is already on the grocery list; not adding".format(item))
 
 
 trello_api = trello.TrelloApi(conf.get()['trello_app_key'])
@@ -142,7 +142,7 @@ trello_db = trellodb.TrelloDB(trello_api, conf.get()['trello_db_board'])
 
 f = open(conf.get()['scanner_device'], 'rb')
 while True:
-    print 'Waiting for scanner data'
+    print('Waiting for scanner data')
 
     # Wait for binary data from the scanner and then read it
     scan_complete = False
@@ -163,7 +163,7 @@ while True:
  
     # Parse the binary data as a barcode
     barcode = parse_scanner_data(scanner_data)
-    print "Scanned barcode '{0}'".format(barcode)
+    print("Scanned barcode '{0}'".format(barcode))
 
     # Match against barcode rules
     barcode_rule = match_barcode_rule(trello_db, barcode)
@@ -175,18 +175,18 @@ while True:
     u = UPCAPI(conf.get()['digiteyes_app_key'], conf.get()['digiteyes_auth_key'])
     try:
         desc = u.get_description(barcode)
-        print "Received description '{0}' for barcode {1}".format(desc, repr(barcode))
+        print("Received description '{0}' for barcode {1}".format(desc, repr(barcode)))
     except urllib2.HTTPError, e:
         if 'UPC/EAN code invalid' in e.msg:
-            print "Barcode {0} not recognized as a UPC; creating learning opportunity".format(repr(barcode))
+            print("Barcode {0} not recognized as a UPC; creating learning opportunity".format(repr(barcode)))
             opp = create_barcode_opp(trello_db, barcode)
-            print "Publishing learning opportunity via SMS"
+            print("Publishing learning opportunity via SMS")
             publish_barcode_opp(opp)
             continue
         elif 'Not found' in e.msg:
-            print "Barcode {0} not found in UPC database; creating learning opportunity".format(repr(barcode))
+            print("Barcode {0} not found in UPC database; creating learning opportunity".format(repr(barcode)))
             opp = create_barcode_opp(trello_db, barcode)
-            print "Publishing learning opportunity via SMS"
+            print("Publishing learning opportunity via SMS")
             publish_barcode_opp(opp)
             continue
         else:
@@ -198,4 +198,4 @@ while True:
         add_grocery_item(trello_api, desc_rule['item'])
         continue
 
-    print "Don't know what to add for product description '{0}'".format(desc)
+    print("Don't know what to add for product description '{0}'".format(desc))
